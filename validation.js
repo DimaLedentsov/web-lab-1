c32ecedbdc062621498cb7aee5ea176ef368c93c
DimaLedentsov <ledentd@mail.ru>
2022-09-03 22:09:33 +0300

    updated php/js

--- validation.js
+++ validation.js
@@ -3,15 +3,20 @@
 
 var form= document.querySelector('.validate_form');
 var validatedButton = document.querySelector('.validate_button');
+var xOptions = document.querySelectorAll(".x");
 var yCoordinate = document.querySelector(".y");
 var rCoordinate = document.querySelector(".R");
-var fields = document.querySelectorAll('.field');
+var resultTable = document.querySelector("#result_table");
 
-
-
+resultTable.append = function(elem){
+  
+}
+function isNumber(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 //функция для генерации ошибок
-var generateTip = function (text, color) { 
+function generateTip(text, color) { 
     var tip = document.createElement('div');
     tip.className = 'tip';
     tip.style.color = color;
@@ -21,48 +26,47 @@ var generateTip = function (text, color) {
 
 
 //функция для очистки подсказок при повторной валидации 
-var removeValidation = function () {
-    var tips = form.querySelectorAll('.tip')      
-    for (var i = 0; i < tips.length; i++) {
-        tips[i].remove()
-    }
+function removeValidation() {
+  var tips = form.querySelectorAll('.tip')      
+  for (var i = 0; i < tips.length; i++) {
+      tips[i].remove()
   }
+}
 
 
-//функция для проверки наличия значений в полях
-var checkFieldsPresence = function () {
-    for (var i = 0; i < fields.length; i++) {
-      if (!fields[i].value) {
-        console.log('field is blank', fields[i])
-        var error = generateTip('The field cannot be empty >:(','red')
-        fields[i].parentElement.insertBefore(error, fields[i])
-        return false;
-      }        
+//функция для проверки наличия выбранной radio кнопки
+function checkSelection(radios) {
+    for(var i=0; i<radios.length; i++){
+      if(radios[i].checked) return true;
     }
-    return true;
+    var error = generateTip('field is blank','red');
+    radios[0].parentElement.insertBefore(error, radios[0]);
+    return false;
 }
 
 // проверка значения в поле на попадание в заданный диапазон
-var validateField = function(coordinate,min,max){
+function validateField(coordinate,min,max){
   if(coordinate.value){
-      if((coordinate.value<=min || coordinate.value>=max || isNaN(coordinate.value))){
-          var error = generateTip('Wrong data =(','red')
+      if(coordinate.value<=min || coordinate.value>=max || !isNumber(coordinate.value)){
+          var error = generateTip('Wrong number format','red')
           coordinate.parentElement.insertBefore(error, coordinate)              
           return false;
       }
       else{
-          var correct = generateTip('Correct data =)','green');
+          var correct = generateTip('Correct data','green');
           coordinate.parentElement.insertBefore(correct, coordinate)              
           return true;              
       }
   }
-  return false
+  var error = generateTip('field is blank','red');
+  coordinate.parentElement.insertBefore(error, coordinate);
+  return false; 
 }
 
 
 // фунция для повторной проверки, что поля заполнены верно, чтобы передать их php скрипту
-function isReady(){
-  return validateField(yCoordinate,-5,5) && validateField(rCoordinate,2,5) && checkFieldsPresence();
+function validateAll(){
+  return checkSelection(xOptions)&&validateField(yCoordinate,-5,5) && validateField(rCoordinate,2,5);
 }  
 
 
@@ -71,16 +75,16 @@ function isReady(){
 $("#inpform").on("submit", function(event){
   event.preventDefault(); 
 
-    console.log("Циферки упали на проверочку =)" );
+    console.log("Got data for check!" );
     console.log('y: ', yCoordinate.value);
     console.log('R: ', rCoordinate.value);
     removeValidation();
 
-    if(!isReady()){
-      console.log("post-запрос отменяется. давай по новой")
+    if(!validateAll()){
+      console.log("post canceled")
       return
     }
-    console.log("начинается отправка данных")
+    console.log("data sending...")
     console.log($(this).serialize());
     $.ajax({
       url: 'script.php',
@@ -88,16 +92,10 @@ $("#inpform").on("submit", function(event){
       data: $(this).serialize() + "&timezone=" + new Date().getTimezoneOffset(),
       dataType: "html",
 
-      headers: { "Accept": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "X-Requested-With": "XMLHttpRequest",
-      "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"},
-    
       success: function(data){
         console.log(data);
         $(".validate_button").attr("disabled", false);	
-        $("#result_table").append(data);
+        
       },
       error: function(error){
         console.log(error);
